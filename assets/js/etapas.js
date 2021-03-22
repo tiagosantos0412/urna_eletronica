03238f4250f62b668eff0c07ddc3af0100c4741e
Tiago Felipe <tiagosantos0412@gmail.com>
2021-03-22 20:01:13 -0300

    Subindo o arquivo etapas.js

---
+++ assets/js/etapas.js
@@ -0,0 +1,50 @@
+ let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38123,
+                nome: 'Edd Beltrano',
+                partido: 'ABC',
+                fotos:[
+                    {url: '3811.jpg', legenda: 'vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Sicrano de Souza',
+                partido: 'DEF',
+                fotos:[
+                    {url: '77222.jpg', legenda: 'vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos:[
+            {
+                numero: 99,
+                nome: 'Beltrano Novais',
+                partido: 'ABC',
+                vice: 'Tião',
+                fotos:[
+                    {url: '99,jpg', legenda:'Prefeito'},
+                    {url: '99_2.jpg', legenda:'Vice-prefeito', small:true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Fulano dos Santos',
+                partido: 'DEF',
+                vice: 'Osmar',
+                fotos:[
+                    {url: '84;jpg', legenda:'Prefeito'},
+                    {url: '84_2,jpg', legenda:'Vice-prefeito', small:true}
+                ]
+            }
+        ]
+    }
+ ];
